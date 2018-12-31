4624088809c68e51f44d4a0884142fd9b594baf1
Mizuki Furusawa <fmzk0326@gmail.com>
2019-01-01 03:36:38 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,45 +1,46 @@
 #!/bin/sh
 
+echo ""
 echo "**************************************************"
 echo "**  Setup powerline fonts...                    **"
 echo "**************************************************"
-echo ""
 git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
 sh ~/fonts/install.sh
 rm -rf ~/fonts
 
 
+echo ""
 echo "**************************************************"
 echo "**  Setup oh-my-zsh...                          **"
 echo "**************************************************"
-echo ""
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.zshrc ~/.zshrc.orig # backup
 curl https://raw.githubusercontent.com/mzkii/dotfiles/master/.zshrc > ~/.zshrc # overwrite
 chsh -s /bin/zsh # change login shell
 
 
+echo ""
 echo "**************************************************"
 echo "**  Setup homebrew...                           **"
 echo "**************************************************"
-echo ""
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/mzkii/dotfiles/master/homebrew.sh)"
 
 
+echo ""
 echo "**************************************************"
 echo "**  Setup cask...                               **"
 echo "**************************************************"
-echo ""
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/mzkii/dotfiles/master/cask.sh)"
 
 
+echo ""
 echo "**************************************************"
 echo "**  Setup mas...                                **"
 echo "**************************************************"
-echo ""
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/mzkii/dotfiles/master/mas.sh)"
 
 
+echo ""
 echo "**************************************************"
 echo "**  DONE!!                                      **"
 echo "**  Please make another setting for iterm2.     **"
